2f386399b01dc86a0dbacf2adb9775fc2ca74414
Haruto K <aoi.stella.dev@gmail.com>
2025-03-23 13:39:57 +0900

    modify initialization of firebase

--- Nimli/Nimli/Applications/NimliApp.swift
+++ Nimli/Nimli/Applications/NimliApp.swift
@@ -12,7 +12,13 @@ import FirebaseCore
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-    FirebaseApp.configure()
+    #if DEBUG
+      if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] != "1" {
+          FirebaseApp.configure()
+      }
+    #else
+      FirebaseApp.configure()
+    #endif
     return true
   }
 }
