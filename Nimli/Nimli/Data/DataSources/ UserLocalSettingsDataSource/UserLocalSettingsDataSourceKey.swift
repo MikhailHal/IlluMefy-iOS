f5e3d95d94bf57ecf6e836f4217fa58f4eabfa6f
Haruto K <aoi.stella.dev@gmail.com>
2025-04-03 01:44:10 +0900

    add userLocalSettingsDataSource

---
+++ Nimli/Nimli/Data/DataSources/ UserLocalSettingsDataSource/UserLocalSettingsDataSourceKey.swift
@@ -0,0 +1,11 @@
+//
+//  UserLocalSettingsDataSourceKey.swift
+//  Nimli
+//
+//  Created by Haruto K. on 2025/04/03.
+//
+
+enum UserLocalSettingsDataSourceKey: String {
+    // swiftlint:disable:next redundant_string_enum_value
+    case isStoreLoginInfomation = "isStoreLoginInfomation"
+}
---
+++ Nimli/Nimli/Data/DataSources/ UserLocalSettingsDataSource/UserLocalSettingsDataSourceProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  UserLocalSettingsDataSourceProtocol.swift
+//  Nimli
+//
+//  Created by Haruto K. on 2025/04/03.
+//
+
+protocol UserLocalSettingsDataSourceProtocol {
+    func getString(forKey key: UserLocalSettingsDataSourceKey, default: String) -> String
+    func setString(_ value: String, forKey key: UserLocalSettingsDataSourceKey)
+    func getBool(forKey key: UserLocalSettingsDataSourceKey) -> Bool
+    func setBool(_ value: Bool, forKey key: UserLocalSettingsDataSourceKey)
+    func remove(forKey key: UserLocalSettingsDataSourceKey)
+}
