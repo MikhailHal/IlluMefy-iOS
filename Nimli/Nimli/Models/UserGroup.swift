8e8d0a98a481a4f7cd92eb180d488e8a41b97139
Haruto K <aoi.stella.dev@gmail.com>
2025-04-07 23:47:00 +0900

    resolve warnings

--- Nimli/Nimli/Models/UserGroup.swift
+++ Nimli/Nimli/Models/UserGroup.swift
@@ -11,6 +11,10 @@ struct UserGroup: Identifiable {
     static let samples = [
         UserGroup(id: "1", name: "家族", memberCount: 4, lastUpdated: Date(), imageUrl: nil),
         UserGroup(id: "2", name: "カップル", memberCount: 2, lastUpdated: Date().addingTimeInterval(-86400), imageUrl: nil),
-        UserGroup(id: "3", name: "ルームメイト", memberCount: 3, lastUpdated: Date().addingTimeInterval(-172800), imageUrl: nil)
+        UserGroup(
+            id: "3",
+            name: "ルームメイト",
+            memberCount: 3,
+            lastUpdated: Date().addingTimeInterval(-172800), imageUrl: nil)
     ]
 }
